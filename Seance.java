c868655efdb66788429f0f1c316295b5d7aba64f
reindeers <velokarri@mail.ru>
2015-12-24 14:13:04 +0300

    Update Seance.java

--- Seance.java
+++ Seance.java
@@ -1,52 +1,66 @@
 public class Seance {
+    int id; // индентификатор
+    private String name; // название фильма
+    private String time; // время фильма
+    private int number; // номер зала
+    private boolean[][] reserved; // таблица резерва
 
-    private int id;
-    private String name;
-    private String time;
-    private int hallNumber;
-    private boolean[][] resSeats;
-
-    public Seance(int id, String name, String time, int hallNumber) {
-        this.id = id;
+    public Seance(String name, String time, Hall hall, int id)
+    {
         this.name = name;
         this.time = time;
-        this.hallNumber = hallNumber;
-
-
+        this.id = id;
+        this.number = hall.getNumber();
+        int i, j, k = hall.getTable().length; //инициализция массива все элементы по умолчанию true
+        this.reserved = new boolean[k][];
+        for (i = 0; i < k; i++) {
+            this.reserved[i] = new boolean[hall.getTable()[i].length];
+            for (j = 0; j < hall.getTable()[i].length; j++) {
+                this.reserved[i][j] = true; //
+            }
+        }
     }
 
-    public int getId() {
+    public String getName(){
+        return this.name;
+    }
+
+    public int getId(){
         return id;
     }
 
-    public String getName() {
-        return name;
+    public void setReserved(int row, int seat){
+        this.reserved[row][seat] = false;
     }
 
-    public String getTime() {
-        return time;
+    public int getNumber() {
+        return number;
     }
 
-    public int getHallNumber() {
-        return hallNumber;
+    public boolean[][] getReserved(){
+        return this.reserved;
     }
 
-    public boolean setRSeats(int row, int seat){
-        resSeats[row][seat] = true;
-        return true;
-    }
 
-    public boolean getRSeats(int row, int seat){
-        return this.resSeats[row][seat];
-    }
-
-    public boolean first(int row, int seat){
-        this.resSeats = new boolean[row][seat];
-        return true;
+    void viewReserve() {
+        String Space[] = new String[]{"   ","  "," ",""};
+        for (int i = 0; i < reserved.length; i++) {
+            System.out.print((i + 1) + ") ");
+            if (this.number == 1) System.out.print(Space[i]); // вставка пробелов в начале строки
+            if (this.number == 3) System.out.print(Space[Space.length-i-1]);
+            for (int j = 0; j < reserved[i].length; j++) {
+                if (reserved[i][j]) System.out.print("O ");
+                else System.out.print("X ");
+            }
+            System.out.println();
+        }
     }
 
     @Override
     public String toString() {
-        return id + ". " + name + ", " + time + ", зал №" + hallNumber;
+        return "Зал " + number + ", " +
+                "Фильм '" + name + "', " +
+                "Время " + time + ", id" + id;
     }
+
 }
